c9307dca218241da6846b5a0687717aeefcc737d
deadbirdscream <89587459+deadbirdscream@users.noreply.github.com>
2023-07-26 12:52:13 +0300

    Basics JS is done

---
+++ 2._basicsJS/2.11_logicOperators/index.js
@@ -0,0 +1,16 @@
+import task1 from './task_2.11.1'
+import task2 from './task_2.11.2'
+import task3 from './task_2.11.3'
+import task4 from './task_2.11.4'
+import task5 from './task_2.11.5'
+// import task6 from './task_2.11.6'
+// import login from './task_2.11.9'
+
+task1()
+task2()
+task3()
+task4()
+task5()
+// task6()
+
+
---
+++ 2._basicsJS/2.11_logicOperators/task_2.11.3.js
@@ -0,0 +1,5 @@
+// Что выведет код ниже?
+
+export default function task3 () {
+  alert( 1 && null && 2 ); // null
+}
